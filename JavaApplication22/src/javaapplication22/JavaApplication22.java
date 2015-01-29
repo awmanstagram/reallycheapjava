276400dc118a304c6537abd2c0b75633f7901108
amccormick2 <amccormick2@HSI211-10.students.wtps.int>
2015-01-29 08:08:46 -0500

    Commit it.

---
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication22;
+
+/**
+ *
+ * @author amccormick2
+ */
+public class JavaApplication22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
